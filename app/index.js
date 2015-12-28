b5a8c0393cfa044ea6fe3eaf2b4543ba95baeaaf
Tidenet <tidenet@tidenetdeiMac.local>
2015-12-28 17:48:07 +0800

    add socket io

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import middleware from './middleware.js';
 import routes from './routes.js';
+import http from 'http';
 
 let app = express();
 let port = process.env.PORT || 3000;
@@ -8,5 +9,9 @@ let port = process.env.PORT || 3000;
 middleware(app);
 routes(app);
 
-app.listen(port);
+let server = http.createServer(app);
+let io = require('socket.io').listen(server);
+io.on('connection', function(){});
+server.listen(port);
+
 console.log('Magic happens on port ' + port);

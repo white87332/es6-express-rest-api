ced31c989c7f5d122338157a22731cac7df66d99
Tidenet <tidenet@tidenetdeiMac.local>
2016-01-19 15:19:11 +0800

    update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,36 +1,49 @@
 import fs from 'fs';
-import util from 'util';
+import { isArray } from 'util';
 import Result from '../class/result';
+import async from 'async';
 
 export default function(app)
 {
-    fs.readdir('./api', (err, files) =>
-    {
-        for (let fileName of files)
-        {
-            let apiFunObj = require('../api/' + fileName).default;
-            let {routes, initExec} = apiFunObj.init();
-            if((initExec !== undefined && !initExec) && (util.isArray(routes) && routes.length > 0))
+    async.series(
+        [
+            function(callback)
             {
-                for (let route of routes)
+                fs.readdir('./api', (err, files) =>
                 {
-                    let url = route.url.toLowerCase();
-                    app[route.method.toLowerCase()](url, apiFunObj.exec);
-                }
-            }
-            else if(initExec !== undefined && initExec)
-            {
-                apiFunObj.exec();
-            }
-        }
+                    for (let fileName of files)
+                    {
+                        let apiFunObj = require('../api/' + fileName).default;
+                        let {routes, initExec} = apiFunObj.init();
+                        if((initExec !== undefined && !initExec) && (isArray(routes) && routes.length > 0))
+                        {
+                            for (let route of routes)
+                            {
+                                let url = route.url.toLowerCase();
+                                app[route.method.toLowerCase()](url, apiFunObj.exec);
+                            }
+                        }
+                        else if(initExec !== undefined && initExec)
+                        {
+                            apiFunObj.exec();
+                        }
+                    }
 
-        app.all('*', function(req, res)
-        {
-            let result = new Result().getResult();
-            result.result = 0;
-            result.message = "404 Not Found";
-            result.data = {};
-            res.json(result);
-        });
-    });
+                    callback();
+                });
+            },
+            function(callback)
+            {
+                app.all('*', function(req, res)
+                {
+                    let result = new Result().getResult();
+                    result.result = 0;
+                    result.message = "404 Not Found";
+                    result.data = {};
+                    res.json(result);
+                });
+
+                callback();
+            }
+        ]);
 }
